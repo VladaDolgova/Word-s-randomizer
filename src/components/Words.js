29cce9d1d556d981c4a093b089e51449ee21adca
Vlada Dolgova <vlada.dolgova@gmail.com>
2023-02-15 21:55:44 +0300

    style tweaks

--- src/components/Words.js
+++ src/components/Words.js
@@ -22,26 +22,28 @@ const Words = () => {
             <div className="row">
                 <h2 style={{ textAlign: 'center' }}>Please enter number of words that you want to check</h2>
             </div>
-            <div className="input-group mt-3 mb-1" style={{ width: '300px' }}>
-                <input type="number" className="form-control" placeholder="Type the  number" aria-label="Type the  number" aria-describedby="button-addon2" onChange={(e) => setNumber(e.target.value)} max={15}/>
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={() => navigate("/table", { state: { number, translation, mode } })}>Let's go!</button>
+            <div className="input-group mt-3 mb-2" style={{ width: '300px' }}>
+                <input type="number" className="form-control" placeholder="Type the  number" aria-label="Type the  number" aria-describedby="button-addon2" onChange={(e) => setNumber(e.target.value)} max={15} />
+                <button className="btn btn-dark" type="button" id="button-addon2" onClick={() => navigate("/table", { state: { number, translation, mode } })}>Let's go!</button>
             </div>
             <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" onChange={() => setTraslation(!translation)} />
+                <input className="form-check-input" style={{ accentColor: 'black' }} type="checkbox" id="inlineCheckbox1" value="option1" onChange={() => setTraslation(!translation)} />
                 <label className="form-check-label" htmlFor="inlineCheckbox1">Without translation</label>
             </div>
-            {modeLabel.map(({ mode, label }) => (
-                <RadioButton
-                    key={mode}
-                    type="radio"
-                    name="flexRadioDefault"
-                    id={"flexRadioDefault" + mode}
-                    handler={() => setMode(mode)}
-                    htmlFor={"flexRadioDefault" + mode}
-                    label={label}
-                    checked={mode === 1 ? true : false}
-                />
-            ))}
+            <div className='form-check form-check-inline mt-2'>
+                {modeLabel.map(({ mode, label }) => (
+                    <RadioButton
+                        key={mode}
+                        type="radio"
+                        name="flexRadioDefault"
+                        id={"flexRadioDefault" + mode}
+                        handler={() => setMode(mode)}
+                        htmlFor={"flexRadioDefault" + mode}
+                        label={label}
+                        checked={mode === 1 ? true : false}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
