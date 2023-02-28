7b3ff4223bcdbea65b4a919003238b35f3fc99c5
Vlada Dolgova <vlada.dolgova@gmail.com>
2023-02-28 21:57:57 +0300

    Add MainPage component + Routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@
 // import './App.css';
 import Words from './Words';
 import Table from './Table';
+import MainPage from './MainPage';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Words />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/words" element={<Words />} />
       <Route path="/table" element={<Table />} />
     </Routes>
   );
---
+++ src/components/MainPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../styles/cover.css";
+
+const MainPage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="text-center text-white">
+            <div className="d-flex w-100 h-100 p-3 mx-auto flex-column flex-wrap position-absolute  bg-dark">
+                <header className="mb-auto mx-5">
+                    <div>
+                        <h3 className="float-md-start mb-0 mr-5">WORDUAGE</h3>
+                        <nav className="nav nav-masthead justify-content-center float-md-end">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" href="randomwords#/words">Dictionary</a>
+                            <a className="nav-link" href="#">Contact</a>
+                        </nav>
+                    </div>
+                </header>
+                <main className="px-3 mb-auto">
+                    <h1>Hello! Choose mode.</h1>
+                    <p className="cover-container mx-auto lead">
+                        Here you can choose one of the application mode. 
+                        <br/>
+                        The first is dictionary, here you can learn new words using categories, levels and tables.
+                        <br/>
+                        The second is testing mode, here you can check your knowledge of English words.
+                        <br/>
+                        Thanks for using this app.
+                    </p>
+                    <button type="button" class="btn btn-lg fw-bold border-white btn-light mx-5" style={{ minWidth: '10em' }} onClick={()=> navigate("/words")}>Dictionary mode</button>
+                    <button type="button" class="btn btn-lg fw-bold border-white btn-light mx-5" style={{ minWidth: '10em' }}>Testing mode</button>
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
